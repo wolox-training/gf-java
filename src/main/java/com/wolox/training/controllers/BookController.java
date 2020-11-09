8a990c542dfb7ed245b8b5a36982cdd0f2815484
gabrielfernandez <fernandezgabriel517@gmail.com>
2020-11-09 14:29:43 -0300

    Added Swagger to UserController and BookController

--- src/main/java/com/wolox/training/controllers/BookController.java
+++ src/main/java/com/wolox/training/controllers/BookController.java
@@ -4,6 +4,10 @@ import com.wolox.training.exceptions.IdMismatchException;
 import com.wolox.training.exceptions.NotFoundException;
 import com.wolox.training.models.Book;
 import com.wolox.training.repositories.BookRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
@@ -19,28 +23,19 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/api/books")
+@Api
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
 
     /**
-     * This method returns views concatenating "Hello" + the name that we send by parameter
-     * @param name: The name to be concatenated in the views
-     * @param model
-     * @return views named greeting.html
-     */
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-
-    /**
      * This method retrieve all books from database and showing them in json format
      * @return Iterable<Book> object to list all books
      */
     @GetMapping
+    @ApiOperation(value = "Return all books", response = Book.class)
+    @ApiResponse(code = 200, message = "Successfully retrieved books")
     public Iterable<Book> getAllBooks(){
         return bookRepository.findAll();
     }
@@ -51,6 +46,11 @@ public class BookController {
      * @return the book with the id passed as parameter
      */
     @GetMapping("/{id}")
+    @ApiOperation(value = "Giving an Id, return the book", response = Book.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved book"),
+            @ApiResponse(code = 404, message = "Book not found")
+    })
     public Book findOne(@PathVariable Long id){
         return bookRepository.findById(id).orElseThrow(NotFoundException::new);
     }
@@ -61,6 +61,11 @@ public class BookController {
      * @return the book with the id passed as parameter
      */
     @GetMapping("/author/{authorName}")
+    @ApiOperation(value = "Giving an author name, return the book", response = Book.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved book"),
+            @ApiResponse(code = 404, message = "Book not found")
+    })
     public Book findByAuthor(@PathVariable String authorName){
         return bookRepository.findByAuthor(authorName).orElseThrow(NotFoundException::new);
     }
@@ -72,6 +77,8 @@ public class BookController {
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Giving a book, save it into database", response = Book.class)
+    @ApiResponse(code = 201, message = "Successfully created book")
     public Book create(@RequestBody Book book) {
         return bookRepository.save(book);
     }
@@ -81,6 +88,11 @@ public class BookController {
      * @param id: Id of the Book to be deleted (Long)
      */
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Giving an id, delete a book from database")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted book"),
+            @ApiResponse(code = 404, message = "Book not found")
+    })
     public void delete(@PathVariable Long id) {
         bookRepository.findById(id).orElseThrow(NotFoundException::new);
         bookRepository.deleteById(id);
@@ -95,6 +107,13 @@ public class BookController {
      * @return the updated Book in the database
      */
     @PutMapping("/{id}")
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Giving an id and a book, update a book in the database", response = Book.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated book"),
+            @ApiResponse(code = 404, message = "Book not found"),
+            @ApiResponse(code = 409, message = "The book id mismatch")
+    })
     public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
         if (book.getId() != id) {
             throw new IdMismatchException();
--- src/main/java/com/wolox/training/controllers/UserController.java
+++ src/main/java/com/wolox/training/controllers/UserController.java
@@ -7,12 +7,17 @@ import com.wolox.training.models.Book;
 import com.wolox.training.models.User;
 import com.wolox.training.repositories.BookRepository;
 import com.wolox.training.repositories.UserRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@Api
 public class UserController {
 
     @Autowired
@@ -22,33 +27,58 @@ public class UserController {
     private BookRepository bookRepository;
 
     @GetMapping
+    @ApiOperation(value = "Return all users", response = User.class)
+    @ApiResponse(code = 200, message = "Successfully retrieved books")
     public Iterable<User> getAllBooks(){
         return userRepository.findAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Giving an Id, return the user", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved user"),
+            @ApiResponse(code = 404, message = "User not found")
+    })
     public User findOne(@PathVariable Long id){
         return userRepository.findById(id).orElseThrow(NotFoundException::new);
     }
 
     @GetMapping("/username/{username}")
+    @ApiOperation(value = "Giving a username, return the user", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved user"),
+            @ApiResponse(code = 404, message = "User not found")
+    })
     public User findByUsername(@PathVariable String username){
         return userRepository.findByUsername(username).orElseThrow(NotFoundException::new);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Giving a user, save it into database", response = User.class)
+    @ApiResponse(code = 201, message = "Successfully created user")
     public User create(@RequestBody User user) {
         return userRepository.save(user);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Giving an Id, delete an user from database")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted user"),
+            @ApiResponse(code = 404, message = "User not found")
+    })
     public void delete(@PathVariable Long id) {
         userRepository.findById(id).orElseThrow(NotFoundException::new);
         userRepository.deleteById(id);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Giving an id and a user, update a book in the database", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated user"),
+            @ApiResponse(code = 404, message = "User not found"),
+            @ApiResponse(code = 409, message = "The user id mismatch")
+    })
     public User updateUser(@RequestBody User user, @PathVariable Long id) {
         if (user.getId() != id) {
             throw new IdMismatchException();
@@ -58,6 +88,13 @@ public class UserController {
     }
 
     @PutMapping("/{userId}/{bookId}")
+    @ApiOperation(value = "Giving a user id and a book id, add a book to the user", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully added book"),
+            @ApiResponse(code = 404, message = "User not found"),
+            @ApiResponse(code = 404, message = "Book not found"),
+            @ApiResponse(code = 208, message = "The book is already owned")
+    })
     public User addBookToUser(@PathVariable Long userId, @PathVariable Long bookId) throws BookAlreadyOwnedException {
         User user = userRepository.findById(userId).orElseThrow(NotFoundException::new);
         Book book = bookRepository.findById(bookId).orElseThrow(NotFoundException::new);
@@ -66,6 +103,13 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}/{bookId}")
+    @ApiOperation(value = "Giving a user id and a book id, remove a book from the user", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted book"),
+            @ApiResponse(code = 404, message = "User not found"),
+            @ApiResponse(code = 404, message = "Book not found"),
+            @ApiResponse(code = 404, message = "The User has not the Book you try to remove")
+    })
     public User removeBookToUser(@PathVariable Long userId, @PathVariable Long bookId) {
         User user = userRepository.findById(userId).orElseThrow(NotFoundException::new);
         Book book = bookRepository.findById(bookId).orElseThrow(NotFoundException::new);
