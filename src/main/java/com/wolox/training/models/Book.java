1830b00fbdb43c7c19517542a9e50767959b56f9
gabrielfernandez <fernandezgabriel517@gmail.com>
2020-11-10 19:00:03 -0300

    Added validation @NotNull into Book model

--- src/main/java/com/wolox/training/models/Book.java
+++ src/main/java/com/wolox/training/models/Book.java
@@ -1,6 +1,7 @@
 package com.wolox.training.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Book {
@@ -9,31 +10,40 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private String genre;
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private String author;
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private String image;
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private String title;
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private String subtitle;
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private String publisher;
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private String year;
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private Integer pages;
 
-    @Column(nullable = false)
+    @Column
+    @NotNull
     private String isbn;
 
     public Book() {
