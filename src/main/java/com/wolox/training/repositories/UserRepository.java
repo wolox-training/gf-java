ec78d478d9e519d6195680ca877caeecfda1e6ad
gabrielfernandez <fernandezgabriel517@gmail.com>
2020-11-13 15:55:07 -0300

    Added complex repository methods into BookRepository and UserRepository

--- src/main/java/com/wolox/training/repositories/UserRepository.java
+++ src/main/java/com/wolox/training/repositories/UserRepository.java
@@ -3,8 +3,11 @@ package com.wolox.training.repositories;
 import com.wolox.training.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     public Optional<User> findByUsername(String username);
+    public List<User> findAllUsersByBirthdateBetweenAndNameContainsIgnoreCase(LocalDate date1, LocalDate date2, String name);
 }
