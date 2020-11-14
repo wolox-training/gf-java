9f442e1961f4e3379c8f630d1b42b568af58fe04
gabrielfernandez <fernandezgabriel517@gmail.com>
2020-11-13 21:22:12 -0300

    Added custom querys to BookRepository and UserRepository

--- src/main/java/com/wolox/training/repositories/BookRepository.java
+++ src/main/java/com/wolox/training/repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.wolox.training.repositories;
 
 import com.wolox.training.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,5 +18,14 @@ import java.util.Optional;
 public interface BookRepository extends JpaRepository<Book, Long> {
     public Optional<Book> findByAuthor(String author);
     public Optional<Book> findByIsbn(String isbn);
-    public List<Book> findAllByPublisherAndGenreAndYear(String publisher, String genre, String year);
+
+   @Query("SELECT b FROM Book b " +
+            "WHERE (:publisher is null OR b.publisher = :publisher) " +
+            "AND (:genre is null OR b.genre = :genre) " +
+            "AND (:year is null OR b.year = :year)")
+    public List<Book> findAllByPublisherAndGenreAndYear(
+            @Param("publisher") String publisher,
+            @Param("genre") String genre,
+            @Param("year") String year);
+
 }
--- src/main/java/com/wolox/training/repositories/UserRepository.java
+++ src/main/java/com/wolox/training/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.wolox.training.repositories;
 
 import com.wolox.training.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,5 +11,14 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     public Optional<User> findByUsername(String username);
-    public List<User> findAllUsersByBirthdateBetweenAndNameContainsIgnoreCase(LocalDate date1, LocalDate date2, String name);
+
+    @Query("SELECT u FROM Users u " +
+            "WHERE (cast(:date1 as date) is null OR u.birthdate >= :date1 " +
+            "AND cast(:date2 as date) is null OR u.birthdate <= :date2) " +
+            "AND (:name = '' OR UPPER(u.name) LIKE UPPER(concat('%', :name,'%')))")
+    public List<User> findAllUsersByBirthdateBetweenAndNameContainsIgnoreCase(
+            @Param("date1") LocalDate date1,
+            @Param("date2") LocalDate date2,
+            @Param("name") String name);
+
 }
