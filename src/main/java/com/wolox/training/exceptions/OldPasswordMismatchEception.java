00c9a3149e9b57fd58a53c499d9b7a227cbe7e5b
gabrielfernandez <fernandezgabriel517@gmail.com>
2020-11-17 15:05:45 -0300

    Created OldPasswordMismatchException wich handles when the current user password and the old password mismatch

--- src/main/java/com/wolox/training/exceptions/OldPasswordMismatchEception.java
+++ src/main/java/com/wolox/training/exceptions/OldPasswordMismatchEception.java
@@ -1,2 +1,14 @@
-package com.wolox.training.exceptions;public class OldPasswordMismatchEception {
+package com.wolox.training.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "The Old Password mismatch")
+public class OldPasswordMismatchEception extends Exception{
+    public OldPasswordMismatchEception() {
+    }
+
+    public OldPasswordMismatchEception(String message) {
+        super(message);
+    }
 }
